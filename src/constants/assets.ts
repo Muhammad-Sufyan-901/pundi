16456d99ca318b33b856a13cced14258d88008ff
Muhammad Sufyan <108451771+Muhammad-Sufyan-901@users.noreply.github.com>
2026-02-07 20:24:38 +0800

    feat : adding static assets for onboarding screen

---
+++ src/constants/assets.ts
@@ -0,0 +1,32 @@
+/**
+ * Centralized image assets constants
+ * Import images here to use throughout the app with proper TypeScript support
+ */
+
+// App Icons
+export const APP_ICONS = {
+  androidBackground: require("../../assets/images/android-icon-background.png"),
+  androidForeground: require("../../assets/images/android-icon-foreground.png"),
+  androidMonochrome: require("../../assets/images/android-icon-monochrome.png"),
+  favicon: require("../../assets/images/favicon.png"),
+  icon: require("../../assets/images/icon.png"),
+  splash: require("../../assets/images/splash-icon.png"),
+} as const;
+
+// Initial/Onboarding Images
+export const ONBOARDING_IMAGES = {
+  coin: require("../../assets/images/onboarding/coint.png"),
+  donut: require("../../assets/images/onboarding/donut.png"),
+  person: require("../../assets/images/onboarding/person.png"),
+  onboarding: require("../../assets/images/onboarding/onboarding.png"),
+} as const;
+
+// All images combined
+export const IMAGES = {
+  appIcons: APP_ICONS,
+  onboarding: ONBOARDING_IMAGES,
+} as const;
+
+// Type exports for TypeScript support
+export type AppIconKey = keyof typeof APP_ICONS;
+export type OnboardingImageKey = keyof typeof ONBOARDING_IMAGES;
---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Centralized constants for the application
+ * Re-export all constants from this barrel file
+ */
+
+export * from "./assets";
